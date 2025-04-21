c95077082dbf553b9b8debe6ff175b25dbd43f8e
flyingking888 <jeremyfeiwang@outlook.com>
2025-04-21 13:35:16 -0400

    4-21-2025

---
+++ mySwiftUITest/ISeeFoodView.swift
@@ -0,0 +1,18 @@
+//
+//  ISeeFoodView.swift
+//  mySwiftUITest
+//
+//  Created by Jeremy Wang on 4/21/25.
+//
+
+import SwiftUI
+
+struct ISeeFoodView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ISeeFoodView()
+}
---
+++ mySwiftUITest/ImagePicker.swift
@@ -0,0 +1,8 @@
+//
+//  ImagePicker.swift
+//  mySwiftUITest
+//
+//  Created by Jeremy Wang on 4/21/25.
+//
+
+import Foundation
